3747418fad4a974c0bb2df641c2abac0fb8031d6
unknown <ryu923can@gmail.com>
2019-09-09 22:40:20 -0400

    Updates

---
+++ precache-manifest.2fae826904b75ec3cee2f5935b8facfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09207354252a6aadba0de4268eb6029b",
+    "url": "/MovieAll/index.html"
+  },
+  {
+    "revision": "286fdcb2023a6dc5e0fc",
+    "url": "/MovieAll/static/css/main.2c7a0f4c.chunk.css"
+  },
+  {
+    "revision": "76e212f9634d6c76cd82",
+    "url": "/MovieAll/static/js/2.47e76761.chunk.js"
+  },
+  {
+    "revision": "286fdcb2023a6dc5e0fc",
+    "url": "/MovieAll/static/js/main.176236a4.chunk.js"
+  },
+  {
+    "revision": "6c4809a499d6480a18b2",
+    "url": "/MovieAll/static/js/runtime~main.075cd735.js"
+  }
+]);
